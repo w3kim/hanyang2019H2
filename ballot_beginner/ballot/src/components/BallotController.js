4cd8d27200e39ee356b476b6843c4788dd761fdf
Eric Woojung Kim <w3kimeric@gmail.com>
2019-11-19 22:30:51 +0900

    voting component

--- ballot_beginner/ballot/src/components/BallotController.js
+++ ballot_beginner/ballot/src/components/BallotController.js
@@ -7,7 +7,9 @@ class BallotController extends React.Component {
         super(props);
         this.state = {
             ballotAddress: '',
-            voterKey: ''
+            voterKey: '',
+            isInvalidBallotEntered: false,
+            isInvalidVoterKeyEntered: false
         };
         this.ballotRef = React.createRef();
     }
@@ -26,27 +28,25 @@ class BallotController extends React.Component {
 
     load = () => {
         // TEST only
-        this.setState({
-            ballotAddress: '0xFD36A83913eB69D3a2b3950507518b8a29905C6F',
-            voterKey: '0x43ddb044e96555cd64fabde34de59712c0ee9bcdbfd927f8ede30641f9ad6bba'
-        });
+        // this.setState({
+        //     ballotAddress: '0xFD36A83913eB69D3a2b3950507518b8a29905C6F',
+        //     voterKey: '0x43ddb044e96555cd64fabde34de59712c0ee9bcdbfd927f8ede30641f9ad6bba'
+        // });
         const { ballotAddress, voterKey } = this.state;
-       
-        if (!caver.utils.isAddress(ballotAddress)) {
-            alert('Invalid contract address');
-            return;
-        }
 
-        if(!caver.utils.isValidPrivateKey(voterKey)) {
-            alert('Invalid private key');
-            return;
-        }
-        
+        this.setState({
+            isInvalidBallotEntered: !caver.utils.isAddress(ballotAddress),
+            isInvalidVoterKeyEntered: !caver.utils.isValidPrivateKey(voterKey)
+        })
+
+        const { isInvalidBallotEntered, isInvalidVoterKeyEntered } = this.state;
+        if (isInvalidBallotEntered || isInvalidVoterKeyEntered) return;
+
         this.ballotRef.current.setup(ballotAddress, voterKey);
     }
 
     render() {
-        const { ballotAddress, voterKey } = this.state;
+        const { ballotAddress, voterKey, isInvalidBallotEntered, isInvalidVoterKeyEntered } = this.state;
         return (
             <div class="section">
                 <div class="container">
@@ -57,33 +57,37 @@ class BallotController extends React.Component {
                                 class="input"
                                 type="text"
                                 placeholder="Ballot contract address"
-                                value={ ballotAddress }
-                                onChange = { this.handleBallotContractAddress }
+                                value={ballotAddress}
+                                onChange={this.handleBallotContractAddress}
                             />
                             <span class="icon is-small is-left">
                                 <i class="fas fa-poll"></i>
                             </span>
                         </div>
+                        {isInvalidBallotEntered &&
+                            <span><p class="is-size-7 has-text-danger">Invalid ballot address entered</p></span>}
                     </div>
-                    
+
                     <div class="field">
                         <label class="label">Voter Key</label>
                         <div class="control has-icons-left">
                             <input
-                                class="input is-success" 
+                                class="input is-success"
                                 type="text"
                                 placeholder="Voter private key"
-                                value={ voterKey }
-                                onChange = { this.handleVoterPrivateKey }
+                                value={voterKey}
+                                onChange={this.handleVoterPrivateKey}
                             />
                             <span class="icon is-small is-left">
                                 <i class="fas fa-user"></i>
                             </span>
-                        </div>                    
+                        </div>
+                        {isInvalidVoterKeyEntered && 
+                            <span><p class="is-size-7 has-text-danger">Invalid voter key entered</p></span>}
                     </div>
-                    <button class='button is-info' onClick = { this.load }>Load</button>
+                    <button class='button is-info is-medium is-fullwidth' onClick={this.load}>Load</button>
                 </div>
-                <Ballot ref={ this.ballotRef } />
+                <Ballot ref={this.ballotRef} />
             </div>
         )
     }
--- ballot_beginner/ballot/src/components/Voting.js
+++ ballot_beginner/ballot/src/components/Voting.js
@@ -8,109 +8,112 @@ class Voting extends React.Component {
             proposals: [],
             selectedProposal: -1,
             voted: false,
-            isVoter: false
+            isVoter: false,
+            isInProgress: false
         }
         this.refresh();
     }
 
-    refresh = async () => {
+    refresh = async (inProgress) => {
         const wallet = caver.klay.accounts.wallet;
         const contract = this.props.contract;
         const n = await contract.methods.getNumProposals().call();
         const v = await contract.methods.voters(wallet[0].address).call();
-        
+
         let p = [];
         for (let i = 0; i < n; i++) {
             p.push(await contract.methods.proposals(i).call());
         }
-        
+
         this.setState({
             proposals: p,
             voted: v.voted,
-            isVoter: v.weight > 0 || v.voted
+            selectedProposal: v.voted ? Number(v.vote) : -1,
+            isVoter: v.weight > 0 || v.voted,
+            isInProgress: inProgress
         });
     }
 
-    onSelectedProposal = (ev) => {
+    onSelectedProposal = (selected) => {
         this.setState({
-            selectedProposal: ev.target.value
+            selectedProposal: selected
         })
     }
 
-    vote = (ev) => {
-        ev.preventDefault();
-        const { selectedProposal } = this.state;
-        const refresh = this.refresh;
-        this.send(this.props.contract.methods.vote(selectedProposal), function(receipt) {
-            refresh();
-        });
+    onReceipt = (receipt) => {
+        console.log(receipt);
+        this.refresh(false);
     }
 
-    send = (txObj, callback) => {
+    onError = (error) => {
+        alert(error.message)
+        console.error(error);
+        this.refresh(false);
+    }
+
+    vote = () => {
+        this.setState({
+            isInProgress: true
+        });
+        this.send(this.props.contract.methods.vote(this.state.selectedProposal), this.onReceipt, this.onError);
+    }
+
+    send = (txObj, okCallback, errCallback) => {
         const wallet = caver.klay.accounts.wallet;
-        txObj.send({
-            from: wallet[0].address,
-            gas: '300000'
-        })
-        .once('transactionHash', console.log)
-        .once('receipt', callback)
-        .once('error', (error) => {
-            alert(error.message)
-            console.error(error);
-        });
+        txObj.send({ from: wallet[0].address, gas: '300000' })
+            .once('transactionHash', console.log)
+            .once('receipt', okCallback)
+            .once('error', errCallback);
     }
 
     render() {
-        const { proposals, selectedProposal, voted, isVoter } = this.state;
+        const { proposals, selectedProposal, voted, isVoter, isInProgress } = this.state;
         if (!isVoter) {
             return (<div>Voter only</div>);
         }
+        let body;
+        if (!isInProgress) {
+            body = <div>
+                <table class="table is-hoverable is-fullwidth">
+                    <thead>
+                        <tr style={{ background: '#eeeeee' }}>
+                            <th>Position</th>
+                            <th>Item</th>
+                            {voted && <th>Votes</th>}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {proposals.map((pr, i) => {
+                            let ret;
+                            if (voted) {
+                                ret = <tr key={i} class={i === selectedProposal && "is-selected"}>
+                                    <td>{i + 1}</td>
+                                    <td>{caver.utils.hexToUtf8(pr.name)}</td>
+                                    {voted && <td>{pr.voteCount}</td>}
+                                </tr>
+                            } else {
+                                ret = <tr key={i} onClick={() => this.onSelectedProposal(i)} class={i === selectedProposal && "is-selected"}>
+                                    <td>{i + 1}</td>
+                                    <td>{caver.utils.hexToUtf8(pr.name)}</td>
+                                    {voted && <td>{pr.voteCount}</td>}
+                                </tr>
+                            }
+                            return (ret);
+                        })}
+                    </tbody>
+                </table>
+                {!voted && <button class='button is-info is-medium is-fullwidth' onClick={this.vote}>Vote</button>}
+            </div>
+        } else {
+            body = <progress class="progress is-large is-primary" max="100">60%</progress>
+        }
+
         return (
             <div class="container">
                 <hr />
                 <div>
-                    <h2 class="subtitle">Proposals</h2>
-                    <table class="table is-hoverable">
-                        <thead>
-                            <tr>
-                                <td>Pos</td>
-                                <td>Item</td>
-                                {voted && <td>Votes</td>}                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { proposals.map((pr, i) => {
-                                // TODO wire onChange event for each table row
-                                let sel = <input type="radio" value={i} 
-                                        selected={ selectedProposal === i} 
-                                        onChange={ this.onSelectedProposal }
-                                        name="proposal" />;
-                                return (<tr key={i}>
-                                    <td>{i}</td>
-                                    <td>{ caver.utils.hexToUtf8(pr.name) }</td>
-                                    {voted && <td>{pr.voteCount}</td>}                                    
-                                </tr>);
-                            })}
-                        </tbody>
-                    </table>
-                    {!voted && <button class="button is-info" value="Vote" />}
-                    {/* <div>
-                        <form onSubmit={ this.vote }>
-                            { proposals.map((pr, i) => {
-                                let sel = <input type="radio" value={i} 
-                                        selected={ selectedProposal === i} 
-                                        onChange={ this.onSelectedProposal }
-                                        name="proposal" />;
-                                return (<div key={i}>
-                                    <span><label>{ caver.utils.hexToUtf8(pr.name) }: </label></span>
-                                    <span>
-                                        {voted ? pr.voteCount : sel}
-                                    </span>
-                                </div>);
-                            })}
-                            {voted ? <input type="submit" value="Vote" disabled /> : <input type="submit" value="Vote" /> }
-                        </form>
-                    </div> */}
+                    <h2 class="subtitle has-text-centered">Proposals</h2>
+                    {body}
                 </div>
             </div>
         )
